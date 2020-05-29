58f4d40e1e26362602c25768331457c720dd2611
romanzemerov <romanzemerov@gmail.com>
2020-05-29 20:50:49 +0300

    fixing ini parsers problems with parsing numbers

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,24 @@
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
+
+const fixIniParse = (obj) => {
+  const keys = Object.keys(obj);
+
+  return keys.reduce((acc, key) => {
+    if (_.isPlainObject(obj[key])) {
+      return { ...acc, [key]: fixIniParse(obj[key]) };
+    }
+
+    if (!Number.isNaN(parseFloat(obj[key]))) {
+      return { ...acc, [key]: Number(obj[key]) };
+    }
+
+    return { ...acc, [key]: obj[key] };
+  }, {});
+};
 
 const parse = (pathName) => {
   const file = fs.readFileSync(pathName, 'utf-8');
@@ -13,7 +30,7 @@ const parse = (pathName) => {
     case '.yml':
       return yaml.safeLoad(file);
     case '.ini':
-      return ini.parse(file);
+      return fixIniParse(ini.parse(file));
     default:
       throw new Error(`Unknown file extension - '${extension}'`);
   }
