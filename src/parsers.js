60ff87cd8460421d1ef2eefc1f6bfcbe9638304f
romanzemerov <romanzemerov@gmail.com>
2020-05-30 20:10:04 +0300

    refactoring getting parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,39 +1,31 @@
 import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const fixIniParse = (obj) => {
-  const keys = Object.keys(obj);
-
-  return keys.reduce((acc, key) => {
-    if (_.isPlainObject(obj[key])) {
-      return { ...acc, [key]: fixIniParse(obj[key]) };
+const formatIni = (object) =>
+  Object.entries(object).reduce((acc, [key, value]) => {
+    if (_.isPlainObject(value)) {
+      return { ...acc, [key]: formatIni(value) };
     }
 
-    if (!Number.isNaN(parseFloat(obj[key]))) {
-      return { ...acc, [key]: Number(obj[key]) };
+    if (!Number.isNaN(parseFloat(value))) {
+      return { ...acc, [key]: Number(value) };
     }
 
-    return { ...acc, [key]: obj[key] };
+    return { ...acc, [key]: value };
   }, {});
-};
 
-const parse = (pathName) => {
-  const file = fs.readFileSync(pathName, 'utf-8');
-  const extension = path.extname(pathName);
-
+const getParser = (extension) => {
   switch (extension) {
     case '.json':
-      return JSON.parse(file);
+      return (file) => JSON.parse(file);
     case '.yml':
-      return yaml.safeLoad(file);
+      return (file) => yaml.safeLoad(file);
     case '.ini':
-      return fixIniParse(ini.parse(file));
+      return (file) => formatIni(ini.parse(file));
     default:
       throw new Error(`Unknown file extension - '${extension}'`);
   }
 };
 
-export default parse;
+export default getParser;
